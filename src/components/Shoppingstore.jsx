0d19c443f1f571ba1095a862e785d659da3f5ff6
mohini-442 <mohinikarwasra598@gmail.com>
2024-01-19 12:59:16 -1200

    commit5

--- src/components/Shoppingstore.jsx
+++ src/components/Shoppingstore.jsx
@@ -132,7 +132,7 @@ const Shoppingstore = () => {
         slidesToShow: 3.11,
         variableWidth: true,
         slidesToScroll: 1,
-        autoplay: true, 
+        autoplay: true,
         responsive: [
             {
                 breakpoint: 1500,
@@ -161,7 +161,7 @@ const Shoppingstore = () => {
                 settings: {
                     slidesToShow: 3,
                     slidesToScroll: 1,
-                    dots:true,
+                    dots: true,
                 },
             },
             {
@@ -177,7 +177,7 @@ const Shoppingstore = () => {
                 settings: {
                     slidesToShow: 1.7,
                     slidesToScroll: 1,
-                    dots:true
+                    dots: true
                 },
             },
             {
@@ -215,45 +215,49 @@ const Shoppingstore = () => {
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
                                 faucibus, urna at pulvinar porta
                             </p>
-                            <div className="!flex gap-[14px] mt-10 justify-center xl:justify-normal !hidden lg:!block">
-                                <button
-                                    className="w-[41px] h-[41px] rounded-[10px] bg-white  flex justify-center items-center hover:bg-gradient1 duration-[0.5s]"
-                                    onClick={() => btn?.current?.slickPrev()}
-                                >
-                                    <span>
-                                        <svg
-                                            width="21"
-                                            height="8"
-                                            viewBox="0 0 21 8"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
+                            <div className="flex gap-[14px] mt-10 justify-center xl:justify-normal">
+                                    <div className="hidden lg:block">
+                                        <button
+                                            className="w-[41px] h-[41px] rounded-[10px] bg-white  flex justify-center items-center hover:bg-gradient1 duration-[0.5s]"
+                                            onClick={() => btn?.current?.slickPrev()}
                                         >
-                                            <path
-                                                d="M0.646446 4.35355C0.451185 4.15829 0.451185 3.84171 0.646446 3.64645L3.82843 0.464466C4.02369 0.269204 4.34027 0.269204 4.53553 0.464466C4.7308 0.659728 4.7308 0.976311 4.53553 1.17157L1.70711 4L4.53553 6.82843C4.7308 7.02369 4.7308 7.34027 4.53553 7.53553C4.34027 7.7308 4.02369 7.7308 3.82843 7.53553L0.646446 4.35355ZM21 4.5H1V3.5H21V4.5Z"
-                                                fill="black"
-                                            />
-                                        </svg>
-                                    </span>
-                                </button>
-                                <button
-                                    className="w-[41px] h-[41px] rounded-[10px] bg-white flex justify-center items-center hover:bg-gradient1 duration-[0.5s]"
-                                    onClick={() => btn?.current?.slickNext()}
-                                >
-                                    <span>
-                                        <svg
-                                            width="21"
-                                            height="8"
-                                            viewBox="0 0 21 8"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
+                                            <span>
+                                                <svg
+                                                    width="21"
+                                                    height="8"
+                                                    viewBox="0 0 21 8"
+                                                    fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                >
+                                                    <path
+                                                        d="M0.646446 4.35355C0.451185 4.15829 0.451185 3.84171 0.646446 3.64645L3.82843 0.464466C4.02369 0.269204 4.34027 0.269204 4.53553 0.464466C4.7308 0.659728 4.7308 0.976311 4.53553 1.17157L1.70711 4L4.53553 6.82843C4.7308 7.02369 4.7308 7.34027 4.53553 7.53553C4.34027 7.7308 4.02369 7.7308 3.82843 7.53553L0.646446 4.35355ZM21 4.5H1V3.5H21V4.5Z"
+                                                        fill="black"
+                                                    />
+                                                </svg>
+                                            </span>
+                                        </button>
+                                    </div>
+                                    <div className="hidden lg:block">
+                                        <button
+                                            className="w-[41px] h-[41px] rounded-[10px] bg-white flex justify-center items-center hover:bg-gradient1 duration-[0.5s]"
+                                            onClick={() => btn?.current?.slickNext()}
                                         >
-                                            <path
-                                                d="M20.3536 4.35355C20.5488 4.15829 20.5488 3.84171 20.3536 3.64645L17.1716 0.464466C16.9763 0.269204 16.6597 0.269204 16.4645 0.464466C16.2692 0.659728 16.2692 0.976311 16.4645 1.17157L19.2929 4L16.4645 6.82843C16.2692 7.02369 16.2692 7.34027 16.4645 7.53553C16.6597 7.7308 16.9763 7.7308 17.1716 7.53553L20.3536 4.35355ZM0 4.5H20V3.5H0V4.5Z"
-                                                fill="black"
-                                            />
-                                        </svg>
-                                    </span>
-                                </button>
+                                            <span>
+                                                <svg
+                                                    width="21"
+                                                    height="8"
+                                                    viewBox="0 0 21 8"
+                                                    fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                >
+                                                    <path
+                                                        d="M20.3536 4.35355C20.5488 4.15829 20.5488 3.84171 20.3536 3.64645L17.1716 0.464466C16.9763 0.269204 16.6597 0.269204 16.4645 0.464466C16.2692 0.659728 16.2692 0.976311 16.4645 1.17157L19.2929 4L16.4645 6.82843C16.2692 7.02369 16.2692 7.34027 16.4645 7.53553C16.6597 7.7308 16.9763 7.7308 17.1716 7.53553L20.3536 4.35355ZM0 4.5H20V3.5H0V4.5Z"
+                                                        fill="black"
+                                                    />
+                                                </svg>
+                                            </span>
+                                        </button>
+                                    </div>
                             </div>
                         </div>
                     </div>
